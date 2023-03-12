6af103a4c28d7dd74b0d19b569f09fdabe3bdafe
abdulloh5007 <abdulloh50007@gmail.com>
2023-03-12 19:07:38 +0500

    readme

---
+++ README.md
@@ -0,0 +1,3 @@
+## This Site can detect your screen mode *#LIGHT* or *#DARK*
+## :grinning:
+### if you are in dark mode , then site`s backgorund will changed to dark color
--- main.js
+++ main.js
@@ -25,8 +25,8 @@ fetch('https://jsonplaceholder.typicode.com/posts/', {
         "Content-type": "application/json",
     },
 })
-.then(res => res.json())
-.then(data => mapper(data))
+    .then(res => res.json())
+    .then(data => mapper(data))
 
 wrapper.style.backgroundColor = '#fff'
 wrapper.style.backgroundSize = 'cover'
@@ -36,7 +36,8 @@ wrapper.style.height = '97vh'
 function mapper(data) {
     loading.style.display = 'block'
     setTimeout(() => {
-        wrapper.style.backgroundImage = 'url("https://i.pinimg.com/564x/1a/55/7e/1a557effc091d5f2ce617211c245671c.jpg")'
+        // wrapper.style.backgroundImage = 'url("https://i.pinimg.com/564x/1a/55/7e/1a557effc091d5f2ce617211c245671c.jpg")'
+        wrapper.style.backgroundColor = 'green'
         wrapper.style.backgroundSize = 'cover'
         wrapper.style.backgroundPosition = 'center'
         wrapper.style.height = '100%'
@@ -56,21 +57,19 @@ function mapper(data) {
 // internet connection
 
 const popup = document.querySelector(".popup"),
-wifiIcon = document.querySelector(".icon i"),
-popupTitle = document.querySelector(".popup .title"),
-popupDesc = document.querySelector(".desc"),
-reconnectBtn = document.querySelector(".reconnect");
+    wifiIcon = document.querySelector(".icon i"),
+    popupTitle = document.querySelector(".popup .title"),
+    popupDesc = document.querySelector(".desc"),
+    reconnectBtn = document.querySelector(".reconnect");
 
 let isOnline = true, intervalId, timer = 10;
 
 const checkConnection = async () => {
     try {
-        // Try to fetch random data from the API. If the status code is between 
-        // 200 and 300, the network connection is considered online 
         const response = await fetch("https://jsonplaceholder.typicode.com/posts");
         isOnline = response.status >= 200 && response.status < 300;
     } catch (error) {
-        isOnline = false; // If there is an error, the connection is considered offline
+        isOnline = false;
     }
     timer = 10;
     clearInterval(intervalId);
@@ -78,29 +77,45 @@ const checkConnection = async () => {
 }
 
 const handlePopup = (status) => {
-    if(status) { // If the status is true (online), update icon, title, and description accordingly
+    if (status) {
         wifiIcon.className = "uil uil-wifi";
         popupTitle.innerText = "Restored Connection";
         popupDesc.innerHTML = "Your device is now successfully connected to the internet.";
         popup.classList.add("online");
         return setTimeout(() => popup.classList.remove("show"), 2000);
     }
-    // If the status is false (offline), update the icon, title, and description accordingly
     wifiIcon.className = "uil uil-wifi-slash";
     popupTitle.innerText = "Lost Connection";
     popupDesc.innerHTML = "Your network is unavailable. We will attempt to reconnect you in <b>10</b> seconds.";
     popup.className = "popup show";
 
-    intervalId = setInterval(() => { // Set an interval to decrease the timer by 1 every second
+    intervalId = setInterval(() => {
         timer--;
-        if(timer === 0) checkConnection(); // If the timer reaches 0, check the connection again
+        if (timer === 0) checkConnection();
         popup.querySelector(".desc b").innerText = timer;
     }, 1000);
 }
 
-// Only if isOnline is true, check the connection status every 3 seconds
 setInterval(() => isOnline && checkConnection(), 3000);
 reconnectBtn.addEventListener("click", () => {
     checkConnection()
     window.location.reload()
 });
+
+// detect screen mode
+
+const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+const detectDarkMode = event => {
+    if (event.matches) {
+        console.log('enabled');
+        wrapper.style.backgroundColor = '#000'
+    } else {
+        console.log('disabled');
+        wrapper.style.backgroundColor = 'green';
+    }
+}
+
+// call detectDarkMode & listen for changes to the user's preferred color scheme
+detectDarkMode(darkModeQuery);
+darkModeQuery.addEventListener('change', e => detectDarkMode(e));
